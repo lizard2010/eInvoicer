7e97c1541fcf38a2b2e5f7d63406a26d1bab4e57
Lizard <lizard.komodo@gmail.com>
2015-02-05 11:30:45 +0200

    Change default route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'activities#index'
+  root 'facts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
